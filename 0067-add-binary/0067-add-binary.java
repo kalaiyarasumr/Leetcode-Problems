00e7016921a1123429642c7b2069f97116c51ed4
kalaiyarasu M R-KSRCT <156080760+kalaiyarasumr@users.noreply.github.com>
2025-04-22 16:15:47 +0530

    Time: 1 ms (99.51%), Space: 42 MB (96.14%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder str = new StringBuilder();
+        int i=a.length()-1;
+        int j = b.length()-1;
+        int carry=0;
+
+        while(i>=0 || j>=0 || carry==1){
+            int total =carry;
+            if(i>=0) total += a.charAt(i--)-'0';
+            if(j>=0) total += b.charAt(j--)-'0';
+            str.append(total%2);
+            carry = total / 2;
+        }
+        return str.reverse().toString();
+    }
+}
